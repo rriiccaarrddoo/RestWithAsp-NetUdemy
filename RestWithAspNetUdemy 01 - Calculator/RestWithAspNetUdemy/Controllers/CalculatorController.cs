c77d0af7eac6f3db7516f4e202782bd8e5be269b
Ricardo Oliveira <ricardo.lima.oliveira@uol.com.br>
2018-07-20 10:29:38 -0300

    Implementing Rest Calculator

---
+++ RestWithAspNetUdemy 01 - Calculator/RestWithAspNetUdemy/Controllers/CalculatorController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RestWithAspNetUdemy.Controllers
+{
+    [Route("api/[controller]")]
+    public class CalculatorController : Controller
+    {
+
+        // GET api/values/5
+        [HttpGet("{firstNumber}/{secondNumber}")]
+        public IActionResult Get(string firstNumber, string secondNumber)
+        {
+
+            if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
+            {
+                var sum = ConvertNumber(firstNumber) + ConvertNumber(secondNumber);
+
+                return Ok(sum.ToString());
+            }
+
+            return BadRequest("Input Invalid.");
+        }
+
+        private decimal ConvertNumber(string number)
+        {
+            decimal convertNumber;
+
+            if (decimal.TryParse(number, out convertNumber))
+            {
+                return convertNumber;
+            }
+
+            return 0;
+        }
+
+        private bool IsNumeric(string number)
+        {
+            decimal convertNumber;
+
+            bool isNumber = decimal.TryParse(number, System.Globalization.NumberStyles.Any, System.Globalization.NumberFormatInfo.InvariantInfo, out convertNumber);
+
+            return isNumber;
+        }
+    }
+}
