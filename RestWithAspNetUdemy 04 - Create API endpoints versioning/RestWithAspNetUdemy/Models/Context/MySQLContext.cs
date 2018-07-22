0889e85608d1503c6e1d7141271be0ff805f35e6
Ricardo Oliveira <ricardo.lima.oliveira@uol.com.br>
2018-07-22 12:30:13 -0300

    Create API endpoints versioning

---
+++ RestWithAspNetUdemy 04 - Create API endpoints versioning/RestWithAspNetUdemy/Models/Context/MySQLContext.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace RestWithAspNetUdemy.Models.Context
+{
+    public class MySQLContext : DbContext
+    {
+        public MySQLContext()
+        {
+
+        }
+
+        public MySQLContext(DbContextOptions options) : base(options) { }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            // get the configuration from the app settings
+
+            var config = new ConfigurationBuilder()
+                 .SetBasePath(Directory.GetCurrentDirectory())
+                 .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                 .Build();
+
+
+            // define the database to use
+            optionsBuilder.UseMySql(config.GetConnectionString("MySQLConnection"));
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Person>().ToTable("Person");
+        }
+
+        public DbSet<Person> People { get; set; }
+
+    }
+}
---
+++ RestWithAspNetUdemy 04 - Create API endpoints versioning/RestWithAspNetUdemy/Services/Implementations/PersonServiceImpl.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using RestWithAspNetUdemy.Models;
+using RestWithAspNetUdemy.Models.Context;
+
+namespace RestWithAspNetUdemy.Services.Implementations
+{
+    public class PersonServiceImpl : IPersonService
+    {
+        private volatile int count;
+        private MySQLContext context;
+
+        public PersonServiceImpl(MySQLContext context)
+        {
+            this.context = context;
+        }
+
+        public Person Create(Person person)
+        {
+            try
+            {
+                context.People.Add(person);
+                context.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+
+            return person;
+        }
+
+        public void Delete(int id)
+        {
+            try
+            {
+                var person = FindById(id);
+
+                if (person != null)
+                {
+                    context.People.Remove(person);
+                    context.SaveChanges();
+                }
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+
+        }
+
+        public IQueryable<Person> FindAll()
+        {
+            IQueryable<Person> people = context.People.Take(10);
+            return people;
+        }
+
+        public Person FindById(int id)
+        {
+            Person person = context.People.SingleOrDefault(p => p.Id == id);
+
+            return person;
+        }
+
+        public Person Update(Person personUpdate)
+        {
+            try
+            {
+                var person = FindById(personUpdate.Id);
+
+                if (person != null)
+                {
+                    context.Entry(person).CurrentValues.SetValues(personUpdate);
+                    context.SaveChanges();
+                }
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+
+            return personUpdate;
+        }
+
+        private string GetGender(int i)
+        {
+            if (i / 2 == 0)
+            {
+                return "Male";
+            }
+
+            return "Female";
+        }
+
+        private int GenerateId()
+        {
+            return Interlocked.Increment(ref count);
+        }
+
+    }
+}
